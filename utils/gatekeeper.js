ccc389c8173bd2fe2429efb77c22da6358687b19
ktmrose <ktm.rose@yahoo.com>
2021-04-06 14:12:32 -0600

    started gatekeeper functions

---
+++ utils/gatekeeper.js
@@ -0,0 +1,13 @@
+const spotifyClient = require('../public/client');
+
+let q = [];
+let costModifier = 0;
+
+function getQLength() {
+    return q.length;
+}
+
+function addSongToQ(trackId, user) {
+
+}
+
