cba339a93fdb455f090a399c2f3c927c6fadffe6
ktmrose <ktm.rose@yahoo.com>
2021-04-06 18:08:25 -0600

    server GUI update

--- utils/gatekeeper.js
+++ utils/gatekeeper.js
@@ -1,10 +1,15 @@
 const spotifyClient = require('../public/client');
 
 let q = [];
+let users = [];
 let costModifier = 0;
 
 function getQLength() {
     return q.length;
+}
+
+function addUser(){
+
 }
 
 function addSongToQ(trackId, user) {
